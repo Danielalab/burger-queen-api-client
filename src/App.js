a3405d5fcf99f21480ef8d40261694e28d90cf5b
Danielalab <olabolacrayola@hotmail.com>
2019-12-28 21:04:05 -0500

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// components
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import logoBurgerQueen from '../images/logo-corona.png';
+
+const Header = () => (
+  <header className="container">
+    <h1 className="font-title text-upper-case px-2 logo-title d-flex align-items-center m-0">
+      Burger Queen
+      <img src={ logoBurgerQueen } alt="logo-burger-queen"
+        className="logo-img"></img>
+    </h1>
+  </header>
+);
+
+export default Header;
