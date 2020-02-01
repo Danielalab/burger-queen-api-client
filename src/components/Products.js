2a96f9c845a3d0b63b12037d296af77aeae0770b
Danielalab <olabolacrayola@hotmail.com>
2020-02-01 17:46:05 -0500

    adding request products data

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavbarMenu from './Navbar-menu';
+import Axios from 'axios';
+
+const requestDataProducts = (token) => {
+  return Axios.get('http://localhost:3001/products', {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
 
 const Products = () => {
+  const token = 'sjhkjfgsafkjs24kdhks'
+  const [dataProducts, setdataProducts] = useState(null);
+  useEffect(() => {
+    requestDataProducts(token)
+  }, [dataProducts]);
+
+  const getDataByCategory = (category) => {
+    requestDataProducts(token)
+  }
+  
   return (
     <div className="w-50">
-      <NavbarMenu></NavbarMenu>
+      <NavbarMenu getData={ getDataByCategory }></NavbarMenu>
     </div>
   )
 }
