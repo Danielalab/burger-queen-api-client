bd8363adb5498f37b536b604c3d736c255236288
Danielalab <olabolacrayola@hotmail.com>
2020-04-18 20:07:08 -0500

    moving HashRouter of AppComponent

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 // components
 import {
-  HashRouter, Switch, Route, Redirect,
+  Switch, Route, Redirect,
 } from 'react-router-dom';
 import TakeOrder from './pages/TakeOrder';
 import Login from './pages/Login';
@@ -12,12 +12,14 @@ import { getToken } from './controllers/auth-data';
 
 function App() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/tomar-orden" render={() => (getToken() === null ? <Redirect to="/" /> : <TakeOrder />)} />
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route
+        exact
+        path="/tomar-orden"
+        render={() => (getToken() === null ? <Redirect to="/" /> : <TakeOrder />)}
+      />
+    </Switch>
   );
 }
 
