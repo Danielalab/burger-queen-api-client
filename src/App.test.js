4cf9076fb3999bc51f0acb6ee3f70c550624cfad
Danielalab <olabolacrayola@hotmail.com>
2020-04-18 19:42:59 -0500

    adding App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import renderWithRouter from './utils/testRouter';
+import * as AuthController from './controllers/auth-data';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Debería renderizar App con la ruta inicial del login', () => {
+  const { getByTestId } = renderWithRouter(<App />);
+  // verificando que se cargue la ruta inicial
+  expect(getByTestId('greeting-login-form')).toBe('Bienvenidxs a \nburger queen');
 });
+
+it('No debería renderizar /tomar-orden sin un token', () => {
+  AuthController.getToken = jest.fn(() => null);
+  const { getByTestId } = renderWithRouter(<App />, { route: '/tomar-orden' });
+  // verificando que siga mostrando la ruta inicial
+  expect(getByTestId('greeting-login-form').textContent).toBe('Bienvenidxs a \nburger queen');
+});
+
+it('Debería renderizar /tomar-orden si existe un token', () => {
+  AuthController.getToken = jest.fn(() => 'tokenfake');
+  const { getByTestId } = renderWithRouter(<App />, { route: '/tomar-orden' });
+  // verificando que navegue a /tomar-orden cuando existe un token
+  expect(getByTestId('text-button-send-order').textContent).toBe('enviar orden');
+});
+
+
+// history.push('/some/bad/route');
