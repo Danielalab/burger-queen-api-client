f301dc150fb76fe383f8f05259410b663caa24b8
Danielalab <olabolacrayola@hotmail.com>
2020-04-25 23:11:02 -0500

    adding mock take-order controller

---
+++ src/controllers/__mocks__/Take-order.js
@@ -0,0 +1,5 @@
+export const filterProductsByCategory = jest.fn();
+export const findProductById = jest.fn();
+export const deleteProduct = jest.fn();
+export const subtractQuantityOfProduct = jest.fn();
+export const getTotalOrder = jest.fn();
