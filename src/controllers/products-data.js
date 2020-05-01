10ba92fbd0b6a79ddeadd6b9fe7fb488732d0568
Danielalab <olabolacrayola@hotmail.com>
2020-04-30 22:33:55 -0500

    refactor(getProductsData): change url && reponse request

--- src/controllers/products-data.js
+++ src/controllers/products-data.js
@@ -1,22 +1,29 @@
 import Axios from 'axios';
 import { getToken } from './auth-data';
 
+const url = 'http://157.245.224.132';
+
 export const getProductsData = () => (
-  Axios.get('http://localhost:3001/products', {
+  Axios.get(`${url}/products`, {
     headers: {
       Authorization: `Bearer ${getToken()}`,
     },
   })
-    .then((response) => response.json())
-    .then((data) => {
-      let result;
-      if (!data.statusCode) {
-        result = data;
-      }
+    .then((response) => response.data)
+    .catch((err) => {
+      const { data } = err.response;
+      let result = {
+        code: data.statusCode,
+      };
       if (data.statusCode === 401) {
         result = {
-          code: 401,
+          ...result,
           message: 'Tu sesión ha expirado, vuelve a iniciar sesión para continuar',
+        };
+      } else {
+        result = {
+          ...result,
+          message: 'Al parecer hubo un error interno, vuelve a intentarlo más tarde',
         };
       }
       return result;
