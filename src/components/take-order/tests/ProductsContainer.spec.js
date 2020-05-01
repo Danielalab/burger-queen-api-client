e26f8dd5bb18a47c844a2c58ef1f8172b93149fa
Danielalab <olabolacrayola@hotmail.com>
2020-04-30 22:25:38 -0500

    test(products-data): adding assertions

--- src/components/take-order/tests/ProductsContainer.spec.js
+++ src/components/take-order/tests/ProductsContainer.spec.js
@@ -10,13 +10,17 @@ jest.mock('../../../controllers/TakeOrder');
 
 const dataFiltered = [
   {
+    _id: '1abc',
     name: 'hamburguesa fake 1',
     price: 5,
+    qty: 2,
     type: 'menu',
   },
   {
+    _id: '3abc',
     name: 'hamburguesa fake 2',
     price: 5,
+    qty: 3,
     type: 'menu',
   },
 ];
---
+++ src/controllers/tests/products-data.spec.js
@@ -0,0 +1,50 @@
+import Axios from 'axios';
+import { getProductsData } from '../products-data';
+
+jest.mock('axios');
+
+describe('getProductsData', () => {
+  const data = [
+    {
+      _id: '1abc',
+      name: 'hamburguesa fake 1',
+      price: 5,
+      type: 'menu',
+    },
+    {
+      _id: '2abc',
+      name: 'cafe con leche fake 1',
+      price: 5,
+      type: 'desayuno',
+    },
+    {
+      _id: '3abc',
+      name: 'hamburguesa fake 2',
+      price: 5,
+      type: 'menu',
+    },
+  ];
+
+  it('Debería retornar un arr de products', async () => {
+    const successResponse = {
+      data,
+    };
+    Axios.get.mockImplementationOnce(() => Promise.resolve(successResponse));
+    const result = await getProductsData();
+    expect(result).toEqual(data);
+  });
+
+  it('Debería retornar un mensaje de error si el request status es 401', async () => {
+    const fakeMessage = 'Tu sesión ha expirado, vuelve a iniciar sesión para continuar.';
+    const errResponse = {
+      response: {
+        data: {
+          statusCode: 401,
+        },
+      },
+    };
+    Axios.get.mockImplementationOnce(() => Promise.reject(errResponse));
+    const result = await getProductsData();
+    expect(result).toEqual(fakeMessage);
+  });
+});
