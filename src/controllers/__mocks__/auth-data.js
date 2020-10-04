9d73420dc83a615d5c446106fb12145f81d8eeb7
Danielalab <olabolacrayola@hotmail.com>
2020-10-04 16:23:22 -0500

    chore(__mocks__): add mock fns for save & get item sessionStorage

--- src/controllers/__mocks__/auth-data.js
+++ src/controllers/__mocks__/auth-data.js
@@ -1,3 +1,5 @@
 export const getAuthToken = jest.fn();
 export const saveToken = jest.fn();
 export const getToken = jest.fn();
+export const saveItemSessionStorage = jest.fn();
+export const getItemSessionStorage = jest.fn();
